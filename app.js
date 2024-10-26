178543f76dce87e23765f2b875efefa866b50cb9
sekhar <munisekharudavalapati1@gmail.com>
2024-10-26 21:49:17 +0530

    complited

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const express=require('express');
 const app=express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors=require('cors')
 
 dotenv.config();
 app.use(express.json());
+app.use(cors()); 
+
+// const adminRoutes=require('') it just for real time application ref
+const userRoutes=require('./routers/user')
 
 
-// const admin=require('') it just for real time application ref
-const user=require('./routers/user')
-
-
-app.use('/admin', adminRoutes);
+// app.use('/admin', adminRoutes);
 app.use('/user', userRoutes);
 
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
     const PORT = process.env.PORT || 5000;
@@ -26,3 +27,11 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error.message);
   });
+//   mongoose.connect(process.env.MONGODB_URI)
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//     app.listen(4000)
+//   })
+//   .catch(err => {
+//     console.error('Error connecting to MongoDB:', err);
+//   });
